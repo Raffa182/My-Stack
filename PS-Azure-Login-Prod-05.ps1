0bf125b4f22f99a6ebb68aec29536030180cec5c
Rafael López <rritsolutions.arg@outlook.com>
2023-06-28 17:19:47 -0300

    Create PS-Azure-Login-Prod-05.ps1

---
+++ PS-Azure-Login-Prod-05.ps1
@@ -0,0 +1,17 @@
+<#
+  .SYNOPSIS
+  Performs monthly data updates.
+
+  .DESCRIPTION
+  The .ps1 script .
+
+  .LINK
+  https://github.com/Raffa182
+
+#>
+
+# Change Title for PG-NA-Enterprise-Prod-05
+$host.ui.RawUI.WindowTitle = "PG-NA-Enterprise-Prod-05"
+
+# Login to PG-NA-Enterprise-Prod-05
+Connect-AzAccount -SubscriptionId "c94548cf-d314-4bd5-abd2-eee92de2aab7"
