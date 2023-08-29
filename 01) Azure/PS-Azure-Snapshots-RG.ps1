f1f51b0078d7d13c56fb7d022a85f935ef41c3be
Rafael López <rritsolutions.arg@outlook.com>
2023-08-29 13:52:17 -0300

    Version 2.0

--- 01) Azure/PS-Azure-Snapshots-RG.ps1
+++ 01) Azure/PS-Azure-Snapshots-RG.ps1
@@ -1,35 +1,43 @@
-# Get-Date para SnapshotName
-$currentDate = Get-Date -Format "yyyyMMdd"
-
 # Cambia el titulo de PS por PG-NA-Enterprise-Prod-05
-$host.ui.RawUI.WindowTitle = "PG-NA-Enterprise-Prod-05"
+#$host.ui.RawUI.WindowTitle = "PG-NA-Enterprise-Prod-05"
 
 # Login PG-NA-Enterprise-Prod-05
-Connect-AzAccount -SubscriptionId "c94548cf-d314-4bd5-abd2-eee92de2aab7"
+#Connect-AzAccount -SubscriptionId "c94548cf-d314-4bd5-abd2-eee92de2aab7"
 
-# Array de las VM dentro de la Subscripcion.
-$vmList = Get-AzVM
+# Backup RG Completo
+$rg_name = "AZ-RG-SAP-LAB-EASTUS"
+$vmList = Get-AzVM -ResourceGroupName $rg_name
+
+#Backup VM Alone
+#$vm_name = ""
+#$vmList = Get-AzVM -Name $vm_name -ResourceGroupName $rg_name
+
+# Get the current date
+$currentDate = Get-Date -Format "yyyyMMdd"
 
 # Aca arranca la magia
 foreach ($vm in $vmList) {
     # Informacion de OS Disk / DataDisks
     $osDisk = $vm.StorageProfile.OsDisk
     $dataDisks = $vm.StorageProfile.DataDisks
+    
 
     # Snapshot del OS Disk
-    $snapshotName = "$($vm.Name)-Backup-OSDisk-$currentDate"
-    New-AzSnapshotConfig -SourceUri $osDisk.ManagedDisk.Id -CreateOption Copy -Location $vm.Location -SnapshotName $snapshotName | New-AzSnapshot
+    $osSnapshotName = "$($vm.Name)-Backup-OSDisk-$currentDate"
+    $osSnapshotConfig = New-AzSnapshotConfig -SourceUri $osDisk.ManagedDisk.Id -CreateOption Copy -Location $vm.Location
+    New-AzSnapshot -Snapshot $osSnapshotConfig -SnapshotName $osSnapshotName -ResourceGroupName $rg_name
 
-    Write-Output "Snaptshot creado para $($vm.Name) - OSDisk"
+    Write-Host "Snapshot creado para $($vm.Name) - OSDisk"
 
     # Snapshot de los DataDisks
     foreach ($dataDisk in $dataDisks) {
-        $snapshotName = "$($vm.Name)-Backup-DataDisk-$($dataDisk.Lun)-$currentDate"
-        New-AzSnapshotConfig -SourceUri $dataDisk.ManagedDisk.Id -CreateOption Copy -Location $vm.Location -SnapshotName $snapshotName | New-AzSnapshot
+        $dataSnapshotName = "$($vm.Name)-Backup-DataDisk-$($dataDisk.Lun)-$currentDate"
+        $dataSnapshotConfig = New-AzSnapshotConfig -SourceUri $dataDisk.ManagedDisk.Id -CreateOption Copy -Location $vm.Location
+        New-AzSnapshot -Snapshot $dataSnapshotConfig -SnapshotName $dataSnapshotName -ResourceGroupName $rg_name
 
-        Write-Output "Snaptshot creado para $($vm.Name) - DataDisk $($dataDisk.Lun)"
+        Write-Host "Snapshot creado para $($vm.Name) - DataDisk $($dataDisk.Lun)"
     }
 }
 
 # See you later Azure!
-Disconnect-AzAccount
+#Disconnect-AzAccount
