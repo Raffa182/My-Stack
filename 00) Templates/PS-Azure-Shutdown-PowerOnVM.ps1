53a7e0bd3028e5a1582227f340d4e6bd202fe889
Rafael López <rritsolutions.arg@outlook.com>
2023-09-05 16:15:21 -0300

    New Template Script for Azure

--- 00) Templates/PS-Azure-Shutdown-PowerOnVM.ps1
+++ 00) Templates/PS-Azure-Shutdown-PowerOnVM.ps1
@@ -1,12 +1,12 @@
 # Define your Azure resource group and VM name
-$resourceGroupName = "YourResourceGroupName"
-$vmName = "YourVMName"
+$vm_name = Read-Host -Prompt 'Input your VM name'
+$rg_name = Read-Host -Prompt 'Input your Resource Group name'
 
 # Shutdown the VM
-Stop-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Force
+Stop-AzVM -ResourceGroupName $rg_name -Name $vm_name -Force
 
 # Wait for the VM to stop (optional, adjust the delay as needed)
 Start-Sleep -Seconds 30
 
 # Start the VM
-Start-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+Start-AzVM -ResourceGroupName $rg_name -Name $vm_name
